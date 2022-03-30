d7a7ae0e766d749c2bccbc325bc07556d20dd3b8
estelson <estelson@gmail.com>
2022-03-30 19:16:09 -0300

    Como desenvolver as telas de configuração de e-mail e senha

--- app/src/main/java/br/com/ejlsistemas/curso/blueshoes/data/AccountSettingsItemsDataBase.kt
+++ app/src/main/java/br/com/ejlsistemas/curso/blueshoes/data/AccountSettingsItemsDataBase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import br.com.ejlsistemas.curso.blueshoes.R
 import br.com.ejlsistemas.curso.blueshoes.domain.AccountSettingItem
 import br.com.ejlsistemas.curso.blueshoes.view.ConfigProfileActivity
+import br.com.ejlsistemas.curso.blueshoes.view.config.connectiondata.ConfigConnectionDataActivity
 
 class AccountSettingsItemsDataBase {
 
@@ -18,7 +19,7 @@ class AccountSettingsItemsDataBase {
             AccountSettingItem(
                 context.getString(R.string.setting_item_login),
                 context.getString(R.string.setting_item_login_desc),
-                ConfigProfileActivity::class.java
+                ConfigConnectionDataActivity::class.java
             ),
 
             AccountSettingItem(
--- app/src/main/java/br/com/ejlsistemas/curso/blueshoes/domain/AccountSettingItem.kt
+++ app/src/main/java/br/com/ejlsistemas/curso/blueshoes/domain/AccountSettingItem.kt
@@ -1,9 +1,9 @@
 package br.com.ejlsistemas.curso.blueshoes.domain
 
-import br.com.ejlsistemas.curso.blueshoes.view.FormActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class AccountSettingItem(
     val label: String,
     val description: String,
-    val activityClass: Class<out FormActivity>
+    val activityClass: Class<out AppCompatActivity>
 )
